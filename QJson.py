5a9ead029e743277b520350b9e04ff32344d49fb
deeredman1991 <deeredman1991@gmail.com>
2016-05-11 08:06:15 -0400

    expanded JDict

--- QJson.py
+++ QJson.py
@@ -1,27 +1,5 @@
-# The MIT License (MIT)
-# 
-# Copyright (c) 2016 deeredman1991
-# 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 import json
-import os.path
+import os
 
 
 
@@ -143,9 +121,38 @@ class JDict(dict):
             _item = super(JDict, self).__getitem__(key)
         return _item
         
-    def __setitem__(self, key, value):
-        super(JDict, self).__setitem__(key, value)
+    def __delitem__(self, key):
+        super(JDict, self).__delitem__(key)
         self.write()
+        
+    def clear(self):
+        super(JDict, self).clear()
+        self.write()
+        
+    def fromkeys(self, seq, value=None):
+        _fromkeys = super(JDict, self).fromkeys(seq, value)
+        self.write()
+        return _fromkeys
+        
+    def pop(self, *args):
+        _pop = super(JDict, self).pop(*args)
+        self.write()
+        return _pop
+        
+    def popitem(self):
+        _popitem = super(JDict, self).popitem()
+        self.write()
+        return _popitem
+        
+    def setdefault(self, key, default=None):
+        _setdefault = super(JDict, self).setdefault(key, default)
+        self.write()
+        return _setdefault
+        
+    def update(self, arg=None, **kwargs):
+        _update = super(JDict, self).update(arg, **kwargs)
+        self.write()
+        return _update
         
     def write(self):
         if self.parent != None and self.filepath == None:
