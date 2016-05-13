def72c99c49def48d4aca1cb060ef83cf0738d06
deeredman1991 <deeredman1991@gmail.com>
2016-05-13 09:38:02 -0400

    added pypi support

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
   name = 'jpyon',
-  packages = ['jpyon'], # this must be the same as the name above
+  packages = ['src'], # this must be the same as the name above
   version = '0.1',
   description = 'Converts objects, lists, and dicts to jsons automatically.',
   author = 'deeredman1991',
